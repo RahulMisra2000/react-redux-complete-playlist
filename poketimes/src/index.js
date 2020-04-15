07a7339b8c9e03fa9b6f2496c37d141dd55a526b
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2020-04-15 01:07:10 -0400

    Creating the Redux Store

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer'        // this is where Net Ninja coded the reducer ****************
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer);   //creating the Redux Store ************************************
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
